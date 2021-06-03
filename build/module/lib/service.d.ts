bee9a968c8de1819de5cc0786372d47cf0c1ba0b
ksnet/09370844 <ozgur.cimen@ext.kocdigital.com>
2021-06-03 18:17:39 +0300

    async config

--- build/module/lib/service.d.ts
+++ build/module/lib/service.d.ts
@@ -1,4 +1,7 @@
 import { AxiosInstance } from 'axios';
+export interface AsyncConfig {
+    get(key: string): Promise<string | null>;
+}
 export interface Service {
     createSecureInstance(baseUrl: string): AxiosInstance;
     createUnsecureInstance(baseUrl: string): AxiosInstance;
@@ -7,11 +10,12 @@ export interface Service {
     setConfig(value: any): any;
     signOut(): any;
     checkUser(): any;
+    getSolution(): string;
+    setTheme(isDark: boolean): void;
+    onThemeChange(isDark: boolean): void;
     store: any;
     vuetify: any;
     dialog: any;
     router: any;
-    getSolution(): string;
-    setTheme(isDark: boolean): void;
-    onThemeChange(isDark: boolean): void;
+    asyncConfig: AsyncConfig;
 }
--- src/lib/service.ts
+++ src/lib/service.ts
@@ -1,18 +1,33 @@
-import {AxiosInstance} from 'axios';
+import { AxiosInstance } from 'axios';
+
+export interface AsyncConfig {
+  get(key: string): Promise<string | null>;
+}
 
 export interface Service {
   createSecureInstance(baseUrl: string): AxiosInstance;
+
   createUnsecureInstance(baseUrl: string): AxiosInstance;
+
   getToken(): string;
+
   getSettings(): any;
+
   setConfig(value: any): any;
+
   signOut();
+
   checkUser(): any;
+
+  getSolution(): string;
+
+  setTheme(isDark: boolean): void;
+
+  onThemeChange(isDark: boolean): void;
+
   store: any;
   vuetify: any;
   dialog: any;
   router: any;
-  getSolution():string;
-  setTheme(isDark: boolean): void
-  onThemeChange(isDark:boolean):void
+  asyncConfig: AsyncConfig;
 }
